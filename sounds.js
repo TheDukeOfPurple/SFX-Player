e2a8cef066d04abd90f6672bc6b4dc50002da80e
smjlg <jacobasmilg@gmail.com>
2019-04-10 20:45:15 -0400

    Removed fade steps column and change fade length to fade duration

--- sounds.js
+++ sounds.js
@@ -1,10 +1,10 @@
 var sounds =
-	[['pg.','Script Ref', 'Name', '', 'Volume', '', 'Fade Length', 'Fade Steps'],	// these are the the column headings - don't change them
+	[['pg.','Script Ref', 'Name', '', 'Volume', '', 'Fade Duration'],	// these are the the column headings - don't change them
 /*		
 	Change this information.  When adding a line, make sure all lines end with a comma.
 	The format below is: page number, dialog or action cue, name of file (must be located in the audio_files folder or the specified path),
 	desired volume, start time (seconds).
 */
-	['ex. pg. #','ex. script ref.','example name','sin44010sec.wav','1','0', '5', '1000'],
+	['ex. pg. #','ex. script ref.','example name','sin44010sec.wav','1','0', '5'],
 
 ];
