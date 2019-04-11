7d8c240ad17e1917572fcdae14aedeefb2a6c7d7
smjlg <jacobasmilg@gmail.com>
2019-04-10 20:44:27 -0400

    Commented, improved readability, and made setting of fade steps automatic

--- scripts/fade.js
+++ scripts/fade.js
@@ -1,22 +1,20 @@
 function fadeout(audioId) {
-	var sound = document.getElementById('sound'+audioId);
+	var sound = document.getElementById('sound'+audioId);	// audio element stored to modify easily
 	var originalVol = sounds[audioId][4];
-	if (originalVol == 1) {originalVol -= .01};
+	if (originalVol == 1) {originalVol -= .01};		// if the volume starts at 100%, it needs to be reduced so that it can be raised to a power
 	var fadeLength = sounds[audioId][6];
-	var fadeSteps = sounds[audioId][7];
-	var stepsElapsed = 0;	
-	var logMult = Math.log(.01)/Math.log(originalVol)/fadeSteps
-	var fade = setInterval(function() {
+	var fadeSteps = fadeLength*60;	//  number of times to decrease the volume within the fadeout. the constant here can be changed to modify the smoothness of the fade.
+	var stepsElapsed = 0;	// iterator
+	var logMult = (Math.log(.01) / Math.log(originalVol)) / fadeSteps;	// algebra to find the correct exponent
+	var fade = setInterval(function() {	// loops each fade step until done fading out
 		sound.volume = Math.pow(originalVol, logMult*stepsElapsed);
 		console.log(sound.volume);
 		if (stepsElapsed >= fadeSteps) {
-			
-			sound.pause();
-			//set the volume back to its predetermined value
-			sound.volume = sounds[audioId][4];
-			clearInterval(fade);
+			sound.pause();	// stop playback
+			sound.volume = sounds[audioId][4];	// set the volume back to its predetermined value
+			clearInterval(fade);	// stop looping
 		}
 		stepsElapsed++;
-	}, fadeLength*1000/fadeSteps);
+	}, fadeLength * 1000 / fadeSteps);
 	
 }
