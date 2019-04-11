83ca6ad482ec26497615114954af03dbb1736b3f
smjlg <jacobasmilg@gmail.com>
2019-04-10 20:29:24 -0400

    Make fadeout logarithmic

--- scripts/fade.js
+++ scripts/fade.js
@@ -1,23 +1,22 @@
 function fadeout(audioId) {
 	var sound = document.getElementById('sound'+audioId);
-	
-	var fadeAudio = setInterval(function () {
-
-		var fadeOutInterval = 0.95;
-		if (sound.volume >= 0.01){
-			// this needs to be logarithmic!!!
-			sound.volume = sound.volume * fadeOutInterval;
-		} else {if (sound.volume > 0) {sound.volume = 0;}}
-		
-		// When volume at zero stop interval "loop"
-		if (sound.volume <= 0) {
-			//pause the playback
+	var originalVol = sounds[audioId][4];
+	if (originalVol == 1) {originalVol -= .01};
+	var fadeLength = sounds[audioId][6];
+	var fadeSteps = sounds[audioId][7];
+	var stepsElapsed = 0;	
+	var logMult = Math.log(.01)/Math.log(originalVol)/fadeSteps
+	var fade = setInterval(function() {
+		sound.volume = Math.pow(originalVol, logMult*stepsElapsed);
+		console.log(sound.volume);
+		if (stepsElapsed >= fadeSteps) {
+			
 			sound.pause();
 			//set the volume back to its predetermined value
 			sound.volume = sounds[audioId][4];
-			//discontinue the intervalling
-			clearInterval(fadeAudio);
+			clearInterval(fade);
 		}
-	}, 40);
+		stepsElapsed++;
+	}, fadeLength*1000/fadeSteps);
 	
 }
