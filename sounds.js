d03a9e43d4aa30a5a945fe6ca861dc1a03110c09
smilg <jacobasmilg@gmail.com>
2020-01-17 10:55:25 -0500

    Fix instruciton comments

--- sounds.js
+++ sounds.js
@@ -2,8 +2,8 @@ var sounds =
 	[['pg.','Script Ref', 'Name', '', 'Volume', '', 'Fade Duration'],	// these are the the column headings - don't change them
 /*		
 	Change this information.  When adding a line, make sure all lines end with a comma.
-	The format below is: page number, dialog or action cue, name of file (must be located in the audio_files folder or the specified path),
-	desired volume, start time (seconds).
+	The format below is: page number, dialog or action cue, name of effect, name of file (must be located in the audio_files folder or the specified path),
+	desired volume, start time (seconds), fade out duration.
 */
 	['ex. pg. #','ex. script ref.','example name','sin44010sec.wav','1','0', '5'],
 
